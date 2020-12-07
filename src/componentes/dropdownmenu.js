689ea4fcd4262117e8c5339233cc6454bd31516a
Rodrigo <rodrigo.guima.ourique@gmail.com>
2020-12-07 10:32:38 -0300

    good cebter

--- src/componentes/dropdownmenu.js
+++ src/componentes/dropdownmenu.js
@@ -19,17 +19,19 @@ constructor(props){
     render(){
         return(
             <div class="navbar">
-                <a onClick={() => this.props.changePage("home")} href="#home">Home</a>
-                <a onClick={() => this.props.changePage("about")} href="#about">Sobre</a>
-                <div class="dropdown">
-                    <button class="dropbtn">Artigos e Trabalhos:
-                        <i class="fa fa-caret-down"></i>
-                    </button>
-                    <div class="dropdown-content">
-                        <a onClick={() => this.props.changePage("english")} href="#ingles">Emprego do Reported Speech em Inglês</a>
-                        <a onClick={() => this.props.changePage("portuguese")} href="#portugues">Podcast Sobre Parnasianismo e Antônio Feijó</a>
-                        <a onClick={() => this.props.changePage("portuguese")} href="#portugues2">Imagem Biográfica de um Autor Simbolista</a>
-                        <a onClick={() => this.props.changePage("spanish")} href="#espanhol">ESPANHOL - PLACEHOLDER</a>
+                <div class="navcontent">
+                    <a onClick={() => this.props.changePage("home")} href="#home">Home</a>
+                    <a onClick={() => this.props.changePage("about")} href="#about">Sobre</a>
+                    <div class="dropdown">
+                        <button class="dropbtn">Artigos e Trabalhos:
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a onClick={() => this.props.changePage("english")} href="#ingles">Emprego do Reported Speech em Inglês</a>
+                            <a onClick={() => this.props.changePage("portuguese")} href="#portugues">Podcast Sobre Parnasianismo e Antônio Feijó</a>
+                            <a onClick={() => this.props.changePage("portuguese2")} href="#portugues2">Imagem Biográfica de um Autor Simbolista</a>
+                            <a onClick={() => this.props.changePage("spanish")} href="#espanhol">ESPANHOL - PLACEHOLDER</a>
+                        </div>
                     </div>
                 </div>
             </div>
